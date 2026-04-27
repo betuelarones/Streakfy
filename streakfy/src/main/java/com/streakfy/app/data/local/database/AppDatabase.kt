e9a7f0d094f0e5c235e8d3e7cc1cdea91d536781
betuelarones <betuelsilva059@gmail.com>
2026-04-26 19:21:08 -0500

    Add tag

--- streakfy/src/main/java/com/streakfy/app/data/local/database/AppDatabase.kt
+++ streakfy/src/main/java/com/streakfy/app/data/local/database/AppDatabase.kt
@@ -5,17 +5,21 @@ import androidx.room.RoomDatabase
 import com.streakfy.app.data.local.dao.FocusSessionDao
 import com.streakfy.app.data.local.dao.StreakDao
 import com.streakfy.app.data.local.dao.TaskDao
+import com.streakfy.app.data.local.entities.FocusSession
+import com.streakfy.app.data.local.entities.StreakRecord
 import com.streakfy.app.data.local.entities.Task
 
 @Database(
-    entities = [Task::class],
+    entities = [
+        Task::class,
+        StreakRecord::class,
+        FocusSession::class
+    ],
     version = 1
 )
-
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun taskDao(): TaskDao
-
     abstract fun streakDao(): StreakDao
-
     abstract fun focusSessionDao(): FocusSessionDao
 }
